ec3ea687b99afb785088b7255c934496de632789
Henok Ali <henokali1@gmail.com>
2018-07-13 15:55:39 +0400

    Toast message added for MSO request.

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,11 @@
-$(".modal").modal({
+$('.modal').modal({
   dismissible: true,
   inDuration: 300,
   outDuration: 200,
   ready: function(modal, trigger) {}
 });
 
-$("deny_modal").modal({
+$('deny_modal').modal({
   dismissible: true,
   inDuration: 300,
   outDuration: 200,
@@ -13,79 +13,84 @@ $("deny_modal").modal({
 });
 
 $(document).ready(function() {
-  $(".dropdown-button").dropdown({
+  $('.dropdown-button').dropdown({
     constrainWidth: false,
     hover: true,
     belowOrigin: true,
-    alignment: "left"
+    alignment: 'left'
   });
 
   // JAVASCRIPT START HERE //
 
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav();
 
-  $(".dropdown-button").dropdown({
+  $('.dropdown-button').dropdown({
     constrainWidth: false,
     hover: true,
     belowOrigin: true,
-    alignment: "left"
+    alignment: 'left'
   });
 
   // JAVASCRIPT START HERE //
 
   // INIT DATEPICKER
-  $(".datepicker").pickadate({
+  $('.datepicker').pickadate({
     selectMonths: true,
     selectYears: 40,
     closeOnSelect: true
   });
 
   // INIT TIMEPICKER
-  $(".timepicker").pickatime({
-    default: "now",
+  $('.timepicker').pickatime({
+    default: 'now',
     twelvehour: true,
-    donetext: "ok",
-    cleartext: "clear",
-    canceltext: "cancel",
+    donetext: 'ok',
+    cleartext: 'clear',
+    canceltext: 'cancel',
     autoclose: true
   });
 
   // INIT SELECT LIST
-  $("select").material_select();
+  $('select').material_select();
 
-  $("a").hover(function() {
-    $(this).css("cursor", "pointer");
+  $('a').hover(function() {
+    $(this).css('cursor', 'pointer');
   });
-  $(".mso_row").hover(function() {
+  $('.mso_row').hover(function() {
     // $(this).css('cursor', 'pointer');
-    $(this).toggleClass("z-depth-5");
+    $(this).toggleClass('z-depth-5');
   });
-  $(".li").hover(function() {
+  $('.li').hover(function() {
     // $(this).css('cursor', 'pointer');
-    $(this).toggleClass("z-depth-5");
+    $(this).toggleClass('z-depth-5');
   });
 
-  $("#comment_modal").click(function() {
-    console.log("Modal Comment Clicke");
+  $('#comment_modal').click(function() {
+    console.log('Modal Comment Clicke');
   });
 
-  $("#cancel_modal").click(function() {
-    console.log("Modal Cancel Clicked");
+  $('#cancel_modal').click(function() {
+    console.log('Modal Cancel Clicked');
   });
 });
 
-$(".approve").click(function(e) {
+$('.approve').click(function(e) {
   // Hide row
   var divId = $(this)
-    .parents(".mso_row")
-    .attr("id");
+    .parents('.mso_row')
+    .attr('id');
   var msoNumber = parseInt(event.target.id);
-  Materialize.toast("MSO-" + msoNumber + " Approved!", 5000);
+  Materialize.toast('MSO-' + msoNumber + ' Approved!', 5000);
   e.preventDefault();
-  $("#" + divId).toggle(1000, "swing", function() {});
+  $('#' + divId).toggle(1000, 'swing', function() {});
 
-  $.getJSON("/approve_mso/" + msoNumber, function(data) {
+  $.getJSON('/approve_mso/' + msoNumber, function(data) {
     //do nothing
   });
   return false;
+});
+
+$('.mso_request_toast').click(function(e) {
+  Materialize.toast(' MSO Requested Successfully', 5000);
+  e.preventDefault();
 });
